cde4ecaec278838daa7939fbba440d356141b271
Taekyu-Na <factualist623@gmail.com>
2025-08-25 21:50:08 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -842,7 +842,29 @@ from Queries
 group by query_name;
 
 ------------------------------------------------------------------------------------------------------------------------
-/* Q20. 
+/* Q20. Monthly Transactions 1 */
+/*
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+Return the result table in any order.
+*/
+
+A20.
+
 
 
 
