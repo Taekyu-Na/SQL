0eac6dc5a914e31ff8186dd559cf1556c435f831
Taekyu-Na <factualist623@gmail.com>
2025-08-26 00:53:45 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -931,7 +931,49 @@ where (customer_id, order_date) IN
 (select customer_id, min(order_date)
 from delivery
 group by customer_id);
+(고객별 첫 주문에 대한 조건을 위해 where 절에 중첩 서브쿼리 넣음)
+(count else null 보다 sum else 0이 더 가독성 좋음)
 
+------------------------------------------------------------------------------------------------------------------------
+/* Q22. Game Play Analysis */
+/*
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places.
+In other words, you need to determine the number of players who logged in on the day immediately following their initial login,
+and divide it by the number of total players.
+*/
+
+A22.
+**오답**
+SELECT round(sum(case when datediff(A.event_date, B.event_date) = 1 then 1 else 0 end)/count(DISTINCT A.player_id), 2) as fraction
+FROM Activity A
+left outer join Activity B
+on A.player_id = B.player_id;
+-> "첫 로그인 날짜 제약 조건 없음"
+
+select round(count(player_id)/(select count(distinct player_id) from activity), 2) as fraction
+FROM Activity
+where (player_id, date_sub(event_date, INTERVAL 1 DAY)) IN
+(select player_id, min(event_date)
+from Activity
+group by player_id)
+(어떤 플레이어가 first_date + 1에 여러 번 로그인했다면, COUNT(player_id)에서 그 플레이어가 여러 번 카운트될 수 있음.
+따라서 분자에도 DISTINCT 넣어주면 좋음. 단, 지문에서 player_id, event_date가 PK로 지정되었기 때문에 내 로직도 문제는 없음)
+
+------------------------------------------------------------------------------------------------------------------------
+/* Q23. Number of Unique Subjects Taught by Each Teacher */
+/*
 
 
 
