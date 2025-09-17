12a9bbe6dae58ec2ca836be612cc6b7bf05384f5
Taekyu-Na <factualist623@gmail.com>
2025-09-17 20:44:22 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -1856,11 +1856,67 @@ SELECT id, count(*) as num
     order by num desc
     limit 1
 
+------------------------------------------------------------------------------------------------------------------------
+/* Q42. Investments in 2016 */
+/*
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| pid         | int   |
+| tiv_2015    | float |
+| tiv_2016    | float |
+| lat         | float |
+| lon         | float |
++-------------+-------+
+pid is the primary key (column with unique values) for this table.
+Each row of this table contains information about one policy where:
+pid is the policyholder's policy ID.
+tiv_2015 is the total investment value in 2015 and tiv_2016 is the total investment value in 2016.
+lat is the latitude of the policy holder's city. It's guaranteed that lat is not NULL.
+lon is the longitude of the policy holder's city. It's guaranteed that lon is not NULL.
+ 
 
+Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
 
+have the same tiv_2015 value as one or more other policyholders, and
+are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
+Round tiv_2016 to two decimal places.
+*/
 
+A42.
+**오답**
+SELECT round(sum(tiv_2016), 2) as tiv_2016
+    from Insurance
+    where pid IN
+        (SELECT pid
+            FROM Insurance
+            group by tiv_2015
+            having count(tiv_2015) > 1
+        UNION
+        SELECT pid
+            FROM Insurance
+            GROUP BY lat, lon
+            having count(*) = 1)
+-> GROUP BY tiv_2015에서 pid를 select 할 수 없음
+-> UNION은 OR 조건이 됨
 
+SELECT round(sum(tiv_2016), 2) as tiv_2016
+    from Insurance
+    where tiv_2015 IN
+        (SELECT tiv_2015
+            FROM Insurance
+            group by tiv_2015
+            having count(tiv_2015) > 1)
+        AND
+        (lat, lon) in
+        (SELECT lat, lon
+            FROM Insurance
+            GROUP BY lat, lon
+            having count(*) = 1);
 
+------------------------------------------------------------------------------------------------------------------------
+/* Q43. Department Top Three Salaries */
+/*
 
 
 
