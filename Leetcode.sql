44d6bc35caf9663193d267e045d0ea674bf25770
Taekyu-Na <factualist623@gmail.com>
2025-09-19 17:08:47 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -1917,11 +1917,51 @@ SELECT round(sum(tiv_2016), 2) as tiv_2016
 ------------------------------------------------------------------------------------------------------------------------
 /* Q43. Department Top Three Salaries */
 /*
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
 
+Table: Department
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
 
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
 
+Write a solution to find the employees who are high earners in each of the departments.
 
+Return the result table in any order.
+*/
+
+A43.
+**아예 못품**
+SELECT b.name as Department,
+       a.name as Employee,
+       a.salary as Salary
+    from Employee A 
+    LEFT JOIN Department B
+    on a.departmentid = b.id
+    WHERE (SELECT count(distinct c.salary)
+            from Employee C
+            where C.departmentid = a.departmentid and c.salary >= a.salary) <= 3
+(서브쿼리: 부서가 같으면서 더 높은 급여 값의 개수를 세기)
+(상위 3 번째 distinct 급여인지 계산)
 
 
 
