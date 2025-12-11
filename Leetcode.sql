3fbf00b28d73eb951ed07706a7c920e0d521db5e
Taekyu-Na <factualist623@gmail.com>
2025-12-11 23:40:58 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -2399,7 +2399,6 @@ SELECT player_id,
 ------------------------------------------------------------------------------------------------------------------------
 /* Q60. Customer Placing the Largest Number of Order
 Table: Orders
-
 +-----------------+----------+
 | Column Name     | Type     |
 +-----------------+----------+
@@ -2408,7 +2407,6 @@ Table: Orders
 +-----------------+----------+
 order_number is the primary key (column with unique values) for this table.
 This table contains information about the order ID and the customer ID.
- 
 
 Write a solution to find the customer_number for the customer who has placed the largest number of orders.
 
@@ -2416,7 +2414,68 @@ The test cases are generated so that exactly one customer will have placed more 
 
 The result format is in the following example.
 */
+A60.
+SELECT customer_number
+    FROM Orders
+        GROUP BY customer_number
+        ORDER BY count(order_number) DESC
+        LIMIT 1
+------------------------------------------------------------------------------------------------------------------------
+/* Q61. Sales Person
+Table: SalesPerson
++-----------------+---------+
+| Column Name     | Type    |
++-----------------+---------+
+| sales_id        | int     |
+| name            | varchar |
+| salary          | int     |
+| commission_rate | int     |
+| hire_date       | date    |
++-----------------+---------+
+sales_id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name and the ID of a salesperson alongside their salary, commission rate, and hire date.
+ 
+Table: Company
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| com_id      | int     |
+| name        | varchar |
+| city        | varchar |
++-------------+---------+
+com_id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name and the ID of a company and the city in which the company is located.
+ 
+Table: Orders
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| order_id    | int  |
+| order_date  | date |
+| com_id      | int  |
+| sales_id    | int  |
+| amount      | int  |
++-------------+------+
+order_id is the primary key (column with unique values) for this table.
+com_id is a foreign key (reference column) to com_id from the Company table.
+sales_id is a foreign key (reference column) to sales_id from the SalesPerson table.
+Each row of this table contains information about one order. This includes the ID of the company, the ID of the salesperson, the date of the order, and the amount paid.
+ 
+Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
 
+Return the result table in any order.
 
+The result format is in the following example.
+*/
+A61.
+SELECT name
+    FROM SalesPerson
+    WHERE sales_id NOT IN (SELECT O.sales_id
+                            FROM Orders O
+                            INNER JOIN Company C
+                            ON O.com_id = C.com_id
+                            WHERE C.name = 'RED')
+------------------------------------------------------------------------------------------------------------------------
+/* Q62. 
 
 
