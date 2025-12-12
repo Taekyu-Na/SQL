6c63b0f696fc00a8fd3fd85e31585bf9ef305f6f
Taekyu-Na <factualist623@gmail.com>
2025-12-12 22:54:11 +0900

    Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -2476,6 +2476,50 @@ SELECT name
                             ON O.com_id = C.com_id
                             WHERE C.name = 'RED')
 ------------------------------------------------------------------------------------------------------------------------
-/* Q62. 
+/* Q62. Tree Node
+Table: Tree
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| p_id        | int  |
++-------------+------+
+id is the column with unique values for this table.
+Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+The given structure is always a valid tree.
+ 
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+Return the result table in any order.
+The result format is in the following example.
+*/
+A62.
+SELECT id,
+       CASE WHEN p_id is null then 'Root'
+            WHEN id NOT IN (SELECT DISTINCT p_id FROM Tree
+                                WHERE p_id is not null) then 'Leaf'
+            ELSE 'Inner' end as type
+    FROM Tree
+'NOT IN (subquery) 구문에서 서브쿼리 결과에 NULL이 하나라도 포함되면 전체 조건이 성립하지 않음
+그 결과는 항상 UNKNOWN(= FALSE 취급)으로 떨어져 기대한 필터링이 전혀 동작하지 않는 경우가 발생'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
