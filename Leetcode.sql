14d150aa6909b3694a0cc34eaeec4784f9031134
Taekyu-Na <factualist623@gmail.com>
2026-01-16 23:34:46 +0900

    Update Leetcode.sql

--- Leetcode.sql
+++ Leetcode.sql
@@ -2472,5 +2472,70 @@ SELECT A.user_id as buyer_id,
         GROUP BY A.user_id, A.join_date
 'EXTRACT는 숫자를 반환하는데 비교는 ''문자열로 되어있음.
 EXTRACT 대신 BETWEEN AND로 하면 더 깔끔'
-
+------------------------------------------------------------------------------------------------------------------------
+/* Q68. Capital Gain/Loss
+Table: Stocks
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| stock_name    | varchar |
+| operation     | enum    |
+| operation_day | int     |
+| price         | int     |
++---------------+---------+
+(stock_name, operation_day) is the primary key (combination of columns with unique values) for this table.
+The operation column is an ENUM (category) of type ('Sell', 'Buy')
+Each row of this table indicates that the stock which has stock_name had an operation on the day operation_day with the price.
+It is guaranteed that each 'Sell' operation for a stock has a corresponding 'Buy' operation in a previous day. It is also guaranteed that each 'Buy' operation for a stock has a corresponding 'Sell' operation in an upcoming day.
+ 
+Write a solution to report the Capital gain/loss for each stock.
+The Capital gain/loss of a stock is the total gain or loss after buying and selling the stock one or many times.
+Return the result table in any order.
+The result format is in the following example.*/
+A68.
+SELECT stock_name,
+       sum(CASE WHEN operation = "Sell" THEN price END)-sum(CASE WHEN operation = "Buy" THEN price END) AS capital_gain_loss
+    FROM Stocks
+        GROUP BY stock_name
+아래도 가능
+SELECT stock_name,
+       sum(CASE WHEN operation = "Buy" THEN - price ELSE price END) AS capital_gain_loss
+    FROM Stocks
+        GROUP BY stock_name
+------------------------------------------------------------------------------------------------------------------------
+/* Q69. Top Travellers
+Table: Users
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the column with unique values for this table.
+name is the name of the user.
+ 
+Table: Rides
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the column with unique values for this table.
+user_id is the id of the user who traveled the distance "distance".
+ 
+Write a solution to report the distance traveled by each user.
+Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
+The result format is in the following example.*/
+A69.
+SELECT A.name,
+       IFNULL(sum(B.distance), 0) as travelled_distance
+    FROM Users A
+    LEFT OUTER JOIN Rides B
+    ON A.id = B.user_id
+        GROUP BY A.id, A.name
+            ORDER BY travelled_distance DESC, A.name
+------------------------------------------------------------------------------------------------------------------------
+/* Q70. 
 
